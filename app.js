2d4530b18ee9c692b45434d1940e07786f87a4a1
Oyebola Odukoya <oyebolaodukoya@Oyebolas-MacBook-Air.local>
2019-07-03 07:03:27 -0400

    Implemented authaurization and users with mongo

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const expressSession = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
 
+const user = require('./models/users');
+const auth = require('./routes/auth');
+
+
+mongoose.connect('mongodb://localhost:27017/ReverseCook', {useNewUrlParser: true});
 ejs.delimiter = '?';
 
 const port = process.env.PORT || 8080;
@@ -12,11 +22,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+app.use(expressSession({
+    secret: "hahahahhaww",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+app.use((req, res, next)=> {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 
-app.get('/', (req, res)=> { res.render('index')});
-
-// Create users
+app.get('/', (req, res)=> { res.render('index') });
+app.use(auth);
+app.get('*', (req, res)=> { res.send('Theres a mistake in the url')} );
 
 
 app.listen(port, () => {
---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const passport = require('passport');
+const user = require('../models/users');
+
+router.get('/register', (req, res)=> {
+    res.render('register');
+});
+
+
+router.post('/register', (req, res)=> {
+    const {username, password} = req.body;
+    const newuser = new user({username});
+    user.register(newuser, password, (err, retuser)=> {
+        if(err){
+            //TODO: Proper error checking
+            console.log(`auth.js router.post ${err}`);
+            return res.render('register');
+        } else {
+            //TODO: Return them to users home page
+            passport.authenticate('local')(req, res, ()=> {
+                res.redirect('/');
+            });
+        }
+    });
+});
+
+
+router.get('/login', (req, res)=> {
+    res.render('login')
+});
+
+//TODO: Error checking proper
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}) ,(req, res)=> {});
+
+router.get('/logout', (req, res)=> {
+    req.logout();
+    res.redirect('/')
+});
+
+module.exports = router;
