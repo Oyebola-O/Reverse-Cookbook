4580d4f30a03d5e0d916ef933f723c7c6d0079bc
Oyebola Odukoya <oyebolaodukoya@Oyebolas-MacBook-Air.local>
2019-07-07 03:36:29 -0400

    Completed most of work with recipe api

---
+++ test/response2.js
@@ -0,0 +1,449 @@
+let response2 = {
+    "vegetarian": true,
+    "vegan": true,
+    "glutenFree": true,
+    "dairyFree": true,
+    "veryHealthy": false,
+    "cheap": false,
+    "veryPopular": true,
+    "sustainable": false,
+    "weightWatcherSmartPoints": 5,
+    "gaps": "no",
+    "lowFodmap": false,
+    "ketogenic": false,
+    "whole30": false,
+    "preparationMinutes": 10,
+    "cookingMinutes": 35,
+    "sourceUrl": "http://www.cookingquinoa.net/quinoa-energy-bars",
+    "spoonacularSourceUrl": "https://spoonacular.com/quinoa-energy-bars-479103",
+    "aggregateLikes": 2483,
+    "spoonacularScore": 55,
+    "healthScore": 5,
+    "creditsText": "Cooking Quinoa",
+    "sourceName": "Cooking Quinoa",
+    "pricePerServing": 56.57,
+    "extendedIngredients": [
+        {
+            "id": 9021,
+            "aisle": "Produce",
+            "image": "apricot.jpg",
+            "consitency": "solid",
+            "name": "apricots",
+            "original": "¼ cup dried apricots, diced",
+            "originalString": "¼ cup dried apricots, diced",
+            "originalName": "dried apricots, diced",
+            "amount": 0.25,
+            "unit": "cup",
+            "meta": [
+                "dried",
+                "diced"
+            ],
+            "metaInformation": [
+                "dried",
+                "diced"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.25,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 59.147,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 93784,
+            "aisle": "Health Foods;Baking",
+            "image": "corn-syrup.png",
+            "consitency": "solid",
+            "name": "brown rice syrup",
+            "original": "5 tablespoons Organic Brown Rice Syrup",
+            "originalString": "5 tablespoons Organic Brown Rice Syrup",
+            "originalName": "Organic Brown Rice Syrup",
+            "amount": 5,
+            "unit": "tablespoons",
+            "meta": [
+                "organic"
+            ],
+            "metaInformation": [
+                "organic"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 5,
+                    "unitShort": "Tbsps",
+                    "unitLong": "Tbsps"
+                },
+                "metric": {
+                    "amount": 5,
+                    "unitShort": "Tbsps",
+                    "unitLong": "Tbsps"
+                }
+            }
+        },
+        {
+            "id": 19071,
+            "aisle": "Health Foods",
+            "image": "chocolate-chips.jpg",
+            "consitency": "solid",
+            "name": "carob chips",
+            "original": "3 tablespoons mini chocolate chips or carob chips (chopped)",
+            "originalString": "3 tablespoons mini chocolate chips or carob chips (chopped)",
+            "originalName": "mini chocolate chips or carob chips (chopped)",
+            "amount": 3,
+            "unit": "tablespoons",
+            "meta": [
+                "mini",
+                "chopped",
+                "()"
+            ],
+            "metaInformation": [
+                "mini",
+                "chopped",
+                "()"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 3,
+                    "unitShort": "Tbsps",
+                    "unitLong": "Tbsps"
+                },
+                "metric": {
+                    "amount": 3,
+                    "unitShort": "Tbsps",
+                    "unitLong": "Tbsps"
+                }
+            }
+        },
+        {
+            "id": 9079,
+            "aisle": "Dried Fruits;Produce",
+            "image": "dried-cranberries.jpg",
+            "consitency": "solid",
+            "name": "dried cranberries",
+            "original": "¼ cup Dried Cranberries",
+            "originalString": "¼ cup Dried Cranberries",
+            "originalName": "Dried Cranberries",
+            "amount": 0.25,
+            "unit": "cup",
+            "meta": [
+                "dried"
+            ],
+            "metaInformation": [
+                "dried"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.25,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 59.147,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 20035,
+            "aisle": "Pasta and Rice;Health Foods",
+            "image": "uncooked-quinoa.png",
+            "consitency": "solid",
+            "name": "quinoa",
+            "original": "½ cup Crispy Brown Rice Cereal or Quinoa Puffs",
+            "originalString": "½ cup Crispy Brown Rice Cereal or Quinoa Puffs",
+            "originalName": "Crispy Brown Rice Cereal or Quinoa Puffs",
+            "amount": 0.5,
+            "unit": "cup",
+            "meta": [
+                "crispy"
+            ],
+            "metaInformation": [
+                "crispy"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.5,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 118.294,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 93816,
+            "aisle": "Cereal",
+            "image": "quinoa-flakes.jpg",
+            "consitency": "solid",
+            "name": "quinoa flakes",
+            "original": "½ cup Quinoa Flakes (oatmeal would work here too)",
+            "originalString": "½ cup Quinoa Flakes (oatmeal would work here too)",
+            "originalName": "Quinoa Flakes (oatmeal would work here too)",
+            "amount": 0.5,
+            "unit": "cup",
+            "meta": [
+                "(oatmeal would work here too)"
+            ],
+            "metaInformation": [
+                "(oatmeal would work here too)"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.5,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 118.294,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 10012014,
+            "aisle": "Savory Snacks",
+            "image": "pumpkin-seeds.jpg",
+            "consitency": "solid",
+            "name": "raw pumpkin seeds",
+            "original": "1/3 cup raw pumpkin seeds (pepitas)",
+            "originalString": "1/3 cup raw pumpkin seeds (pepitas)",
+            "originalName": "raw pumpkin seeds (pepitas)",
+            "amount": 0.3333333333333333,
+            "unit": "cup",
+            "meta": [
+                "raw",
+                "(pepitas)"
+            ],
+            "metaInformation": [
+                "raw",
+                "(pepitas)"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.333,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 78.863,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 12585,
+            "aisle": "Nuts;Savory Snacks",
+            "image": "cashews.jpg",
+            "consitency": "solid",
+            "name": "salted cashews",
+            "original": "1 cup salted cashews, chopped",
+            "originalString": "1 cup salted cashews, chopped",
+            "originalName": "salted cashews, chopped",
+            "amount": 1,
+            "unit": "cup",
+            "meta": [
+                "salted",
+                "chopped"
+            ],
+            "metaInformation": [
+                "salted",
+                "chopped"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 1,
+                    "unitShort": "cup",
+                    "unitLong": "cup"
+                },
+                "metric": {
+                    "amount": 236.588,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        }
+    ],
+    "id": 479103,
+    "title": "Quinoa Energy Bars",
+    "readyInMinutes": 45,
+    "servings": 12,
+    "image": "https://spoonacular.com/recipeImages/479103-556x370.jpg",
+    "imageType": "jpg",
+    "cuisines": [],
+    "dishTypes": [
+        "side dish"
+    ],
+    "diets": [
+        "gluten free",
+        "dairy free",
+        "lacto ovo vegetarian",
+        "vegan"
+    ],
+    "occasions": [],
+    "winePairing": {},
+    "instructions": "Preheat oven to 325 degrees.  Line an 8-inch square baking pan with foil and spray well with cooking spray.  (Don’t try to go lightly here – I did and they stuck more than I would have liked.)In a large bowl combine cashews, brown rice cereal, quinoa flakes, pumpkin seeds, cranberries, apricots and chocolate chips.  Stir until well combined.  Add brown rice syrup and stir gently until combined.  Transfer mixture to prepared baking dish and spread until even.  Coat another piece of foil with cooking spray and place, spray side down, on top of the mixture.  Place another pan on top and press down firmly to compress the mixture.  Remove pan and top piece of tin foil.Place in a preheated oven and cook until the edges have turned golden – about 24 minutes for a metal pan and 35 minutes for a glass pan.  Cool the pan on a wire rack for 10 minutes.Remove from pan and cut into 12 pieces.  Allow to cool completely before removing foil and serving.",
+    "analyzedInstructions": [
+        {
+            "name": "",
+            "steps": [
+                {
+                    "number": 1,
+                    "step": "Preheat oven to 325 degrees.  Line an 8-inch square baking pan with foil and spray well with cooking spray.  (Don’t try to go lightly here – I did and they stuck more than I would have liked.)In a large bowl combine cashews, brown rice cereal, quinoa flakes, pumpkin seeds, cranberries, apricots and chocolate chips.  Stir until well combined.",
+                    "ingredients": [
+                        {
+                            "id": 12014,
+                            "name": "pumpkin seeds",
+                            "image": "pumpkin-seeds.jpg"
+                        },
+                        {
+                            "id": 93816,
+                            "name": "quinoa flakes",
+                            "image": "quinoa-flakes.jpg"
+                        },
+                        {
+                            "id": 9021,
+                            "name": "apricot",
+                            "image": "apricot.jpg"
+                        }
+                    ],
+                    "equipment": [
+                        {
+                            "id": 404646,
+                            "name": "baking pan",
+                            "image": "roasting-pan.jpg"
+                        },
+                        {
+                            "id": 404783,
+                            "name": "bowl",
+                            "image": "bowl.jpg"
+                        },
+                        {
+                            "id": 404765,
+                            "name": "aluminum foil",
+                            "image": "aluminum-foil.png"
+                        },
+                        {
+                            "id": 404784,
+                            "name": "oven",
+                            "image": "oven.jpg"
+                        }
+                    ]
+                },
+                {
+                    "number": 2,
+                    "step": "Add brown rice syrup and stir gently until combined.",
+                    "ingredients": [
+                        {
+                            "id": 93784,
+                            "name": "brown rice syrup",
+                            "image": "corn-syrup.png"
+                        }
+                    ],
+                    "equipment": []
+                },
+                {
+                    "number": 3,
+                    "step": "Transfer mixture to prepared baking dish and spread until even.  Coat another piece of foil with cooking spray and place, spray side down, on top of the mixture.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 404646,
+                            "name": "baking pan",
+                            "image": "roasting-pan.jpg"
+                        },
+                        {
+                            "id": 404765,
+                            "name": "aluminum foil",
+                            "image": "aluminum-foil.png"
+                        }
+                    ]
+                },
+                {
+                    "number": 4,
+                    "step": "Place another pan on top and press down firmly to compress the mixture.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "pan.png"
+                        }
+                    ]
+                },
+                {
+                    "number": 5,
+                    "step": "Remove pan and top piece of tin foil.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 404765,
+                            "name": "aluminum foil",
+                            "image": "aluminum-foil.png"
+                        },
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "pan.png"
+                        }
+                    ]
+                },
+                {
+                    "number": 6,
+                    "step": "Place in a preheated oven and cook until the edges have turned golden – about 24 minutes for a metal pan and 35 minutes for a glass pan.  Cool the pan on a wire rack for 10 minutes.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 405900,
+                            "name": "wire rack",
+                            "image": "wire-rack.jpg"
+                        },
+                        {
+                            "id": 404784,
+                            "name": "oven",
+                            "image": "oven.jpg"
+                        },
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "pan.png"
+                        }
+                    ],
+                    "length": {
+                        "number": 69,
+                        "unit": "minutes"
+                    }
+                },
+                {
+                    "number": 7,
+                    "step": "Remove from pan and cut into 12 pieces.  Allow to cool completely before removing foil and serving.",
+                    "ingredients": [],
+                    "equipment": [
+                        {
+                            "id": 404765,
+                            "name": "aluminum foil",
+                            "image": "aluminum-foil.png"
+                        },
+                        {
+                            "id": 404645,
+                            "name": "frying pan",
+                            "image": "pan.png"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+module.exports = response2;
