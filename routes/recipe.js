900c6eadce4cc0c708de345e60d79bd952e7f745
Oyebola Odukoya <oyebolaodukoya@Oyebolas-MacBook-Air.local>
2019-07-06 23:49:58 -0400

    Advanced design for viewing search results in results dot ejs

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const router = express.Router({mergeParams: true});
 const recipe = require('../models/recipe');
+const response = require('../test/response');
 
 
 router.get('/search', (req, res)=> {
@@ -11,33 +12,34 @@ router.get('/search', (req, res)=> {
 
 
 router.post('/search/get', (req, res)=> {
-    const ingredients = req.body.ingredients.ing;
-    const number = 5, ranking = 1, ignorePantry = "true";
-    let ingstr = "";
-    ingredients.forEach(ing => {
-        ingstr === "" ? ingstr+= `${ing}` :  ingstr+= `%2C${ing}`
-    });
+    res.render('recipe/results' ,{response});
+    // const ingredients = req.body.ingredients.ing;
+    // const number = 4, ranking = 1, ignorePantry = "true";
+    // let ingstr = "";
+    // ingredients.forEach(ing => {
+    //     ingstr === "" ? ingstr+= `${ing}` :  ingstr+= `%2C${ing}`
+    // });
     
-    const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=${number}&ranking=${ranking}&ignorePantry=${ignorePantry}&ingredients=${ingstr}`;
-    const object = {
-        headers: {
-            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-            'X-RapidAPI-Key': 'd22f337e00mshc681b77232bbac7p127fdejsnbdefb85b6ece'
-        }
-    }
+    // const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=${number}&ranking=${ranking}&ignorePantry=${ignorePantry}&ingredients=${ingstr}`;
+    // const object = {
+    //     headers: {
+    //         'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+    //         'X-RapidAPI-Key': 'd22f337e00mshc681b77232bbac7p127fdejsnbdefb85b6ece'
+    //     }
+    // }
 
-    fetch(url, object).then(response => response.json())
-    .then((response)=> {
-        res.render('recipe/results' ,response);
-    })
+    // fetch(url, object).then(response => response.json())
+    // .then((response)=> {
+    //     res.render('recipe/results' ,{response});
+    // })
     
-    .catch((error)=> {
-        if(error){
-            //TODO: Handle error properly
-            console.log(error);
-            res.send('There was an error recipe.js router.post search/get')
-        }
-    });
+    // .catch((error)=> {
+    //     if(error){
+    //         //TODO: Handle error properly
+    //         console.log(error);
+    //         res.send('There was an error recipe.js router.post search/get')
+    //     }
+    // });
 });
 
 
